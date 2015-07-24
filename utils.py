2e3b2651f327a84c7c3bf93ee08cbafa924bd4bd
Keane O'Kelley <kokelley@iastate.edu>
2015-07-24 09:06:38 -0500

    Postback is working correctly now

--- utils.py
+++ utils.py
@@ -5,20 +5,10 @@ def num(s):
     try:
         return int(s)
     except (ValueError, TypeError):
-        return None
+        return 0
 
 def generate_session_id():
 	return str(random.randint(1,2000)) + 'test'
 
 def get_salted_password(password):
 	return password + 'sajd134kj2rv423J2z3@$#mnfmdj3m2Dn3ehfjdnklm$@#REKGlhjkJFDcdsjkh'
-
-def do_postback(txid, postback):
-    # Do asynchronously with futures so the user doesn't have to wait.
-    session = FuturesSession()
-    url = 'http://' + postback
-    data = {
-            'txid': txid,
-            'paid': 'TRUE',
-            }
-    r = session.post(url, data=data)
